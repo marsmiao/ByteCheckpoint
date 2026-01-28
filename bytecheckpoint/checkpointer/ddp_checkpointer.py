cc60c86a378e683a25d40882e12ed1e18d0b7e0f
marshall.miao <marshall.miao@spirit-ai.com>
2026-01-28 02:49:51 +0000

    relax torch version check to 2.1+ for checkpointers

--- bytecheckpoint/checkpointer/ddp_checkpointer.py
+++ bytecheckpoint/checkpointer/ddp_checkpointer.py
@@ -87,8 +87,8 @@ class DDPCheckpointer(BaseCheckpointer):
         logger.info("Start save function call. ")
 
         # Check version.
-        assert torch.__version__.strip() < "2.6.0" and torch.__version__.strip() >= "2.1.0", (
-            "ByteCheckpoint now only support torch version from 2.1 to 2.5"
+        assert "2.1.0" <= torch.__version__.strip(), (
+            "ByteCheckpoint now only support torch version from 2.1 and above"
         )
         # Check role.
         if role and role not in SUPPORTED_ROLE_SUFFIX_TYPES:
